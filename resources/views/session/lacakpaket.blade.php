acdb885bd7ce063f957144beb0b76701ad050af1
haziqdafren <haziqdafren20@gmail.com>
2024-12-01 16:29:25 +0700

    haziq ubah

--- resources/views/session/lacakpaket.blade.php
+++ resources/views/session/lacakpaket.blade.php
@@ -8,10 +8,12 @@
       <div class="container">
         <div class="row">
           <div class="col-xl-6 col-lg-7 col-md-8 d-flex flex-column mx-auto">
-            <div class="card card-plain mt-8">
+            <div class="card card-plain mt-4"> <!-- Adjusted margin-top -->
               <div class="card-header pb-0 text-left bg-transparent">
-                <h3 class="font-weight-bolder text-info text-gradient">Lacak Paket</h3>
-                <p class="mb-0">Masukkan nomor resi atau detail terkait untuk mencari data paket Anda.</p>
+                <div class="mb-4"> <!-- Added a margin bottom for spacing -->
+                  <h3 class="font-weight-bolder text-info text-gradient">Lacak Paket</h3>
+                  <p class="mb-0">Masukkan nomor resi atau detail terkait untuk mencari data paket Anda.</p>
+                </div>
               </div>
               <div class="card-body">
                 <form role="form" method="GET" action="{{ url('search-paket') }}">
@@ -53,5 +55,12 @@
               @else
               <p class="text-center mt-4">Tidak ada data ditemukan.</p>
               @endif
-</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
+
 @endsection
