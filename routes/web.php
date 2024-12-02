a5434ac128f121e8b63978774fc2674d4e1b893a
nazwa23ti@mahasiswa.pcr.ac.id <nazwa23ti@mahasiswa.pcr.ac.id>
2024-12-02 11:46:41 +0700

    udh siap tambah data

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/paket', function () {
+    return view('paket'); // Pastikan file 'paket.blade.php' ada di resources/views
+});
 
 Route::group(['middleware' => 'auth'], function () {
 
